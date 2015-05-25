67563614f46a8e662162a6fac0313e345c07f171
ZHUZIRUN <zirunzhu@gmail.com>
2015-05-25 18:00:15 +0900

    add date log infor"

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ exports.index = function(req, res){
   res.render("index.html");
   var ip = req.connection.remoteAddress || (req.headers['x-forwarded-for'] || '').split(',')[0];
   console.log(ip);
-  var str = "IP: " + ip + "\n";
+  var str = "date: " + new Date() + "    ip: " + ip + "\n";
   fs.appendFile("access.log", str, function(err, result){});
 
 };
